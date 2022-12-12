b1e9660b72a7ce41ec6533056e15a9b54e1cce6a
ToQuery <toquery@qq.com>
2022-12-12 21:45:12 +0800

    init

---
+++ src/main/java/io/github/toquery/example/spring/boot/querydsl/ExampleSpringBootQuerydslApplication.java
@@ -0,0 +1,13 @@
+package io.github.toquery.example.spring.boot.querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringBootQuerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringBootQuerydslApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/toquery/example/spring/boot/querydsl/ExampleSpringBootQuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.toquery.example.spring.boot.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringBootQuerydslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
